3d1ab799e45713ef436c14344c0f961074f168dd
FaezeK <keshavarz.f10@gmail.com>
2017-11-14 11:47:00 -0700

    Add files via upload

---
+++ Classes.java
@@ -0,0 +1,18 @@
+package andTreeSearch;
+
+public class Classes {
+
+	String discipline;
+	int course_num;
+	String class_type;
+
+	public String getDiscipline(){
+		return discipline;
+	}
+	public int getCourseNum(){
+		return course_num;
+	}
+	public String getClassType(){
+		return class_type;
+	}
+}
---
+++ Slot.java
@@ -0,0 +1,26 @@
+package andTreeSearch;
+
+public class Slot {
+
+	String type;
+	String day;
+	String time;
+	int max;
+	int min;
+
+	public String getType(){
+		return type;
+	}
+	public String getDay(){
+		return day;
+	}
+	public String getTime(){
+		return time;
+	}
+	public int getMin(){
+		return min;
+	}
+	public int getMax(){
+		return max;
+	}
+}
